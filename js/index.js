57e0682246d20fbc9678350d7b578c0c0f1ff039
PrietoAlejandro <faospitia2@misena.edu.co>
2022-11-23 13:51:29 -0500

    evidencia final

--- js/index.js
+++ js/index.js
@@ -1,34 +1,35 @@
-function juanyfabio() {
-
+function jugar() 
+{
         const random = Math.floor(Math.random() * 9);
         const random1 = Math.floor(Math.random() * 9);
         
         console.log(random)
         console.log(random1)
     
-        document.querySelector(".balota").textContent = `${random}`;
-        document.querySelector(".balota1").textContent = `${random1}`;
+        document.querySelector(".balota1").textContent = `${random}`;
+        document.querySelector(".balota2").textContent = `${random1}`;
     
-        let f = document.querySelector(".opcion1");
-        let j = document.querySelector(".opcion2");
+        let uno = document.querySelector(".opcion1");
+        let dos = document.querySelector(".opcion2");
     
-        console.log(f)
+        console.log(uno)
     
-        let fabian = (f.value);
-        let juandiego = (j.value);
+        let seleccion1 = (uno.value);
+        let seleccion2 = (dos.value);
     
     
-        if ((fabian == random) && (juandiego == random1)) {
-            alert("winner");
-    
-        } else {
-            alert("a bienestar");
-        }
-    
+        if ((seleccion1 == random) && (seleccion2 == random1)) 
+        {
+            alert("Usted ha ganado, ha coincidido con la maquina");    
+        } 
+        else
+        {
+            alert("Usted ha perdido, el resultado de la maquina es diferente");
+        }    
     }
     
-    const boton = document.querySelector(".play");
+    const boton = document.querySelector(".Jugarr");
     
-    boton.addEventListener("click", juanyfabio);
+    boton.addEventListener("click", jugar);
     
     
